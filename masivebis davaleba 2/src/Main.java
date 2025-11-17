591ba90f45bf9c0d55657e7d88428d4f3afc810d
anamaghr <maghradzeana1@gmail.com>
2025-11-17 17:59:36 +0400

    Add files via upload

---
+++ masivebis davaleba 2/src/Main.java
@@ -0,0 +1,109 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        // N1
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int n = in.nextInt();
+//        int[] arr = {1,2,3,4,5};
+//        for (int i = 1; i < arr.length; i++) {
+//            if(n == arr[i]){
+//                arr[i] = 0;
+//                System.out.println(Arrays.toString(arr));
+//            }
+//        }
+
+        // N2
+
+//        int[] arr = {4,5,8,12,56};
+//        int min = arr[0];
+//        int max = arr[0];
+//        int dayToBuy = 0;
+//        int dayToSell = 0;
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            if(min > arr[i])
+//            {
+//                min = arr[i];
+//            }
+//
+//        }
+//
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            if(arr[i] == min)
+//            {
+//               dayToBuy  = i;
+//            }
+//
+//        }
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            if(max < arr[i])
+//            {
+//                max = arr[i];
+//            }
+//
+//        }
+//
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            if(arr[i] == max)
+//            {
+//                dayToSell  = i;
+//            }
+//
+//        }
+//
+//        System.out.println("Best day to buy is on " + dayToBuy + " day, for " + min + " lari");
+//        System.out.println("Best day to sell is on " + dayToSell  + " day, for " + max + " lari");
+
+
+        //  N3
+        
+//        int[] arr = {1,2,0,7,1};
+//        boolean isPalindrome = true;
+//        for (int i = 0; i < arr.length ; i++) {
+//            if(arr[i] != arr[arr.length - i - 1])
+//            {
+//                isPalindrome = false;
+//                break;
+//            }
+//        }
+//
+//        if (isPalindrome) {
+//            System.out.println("array " + Arrays.toString(arr) + " is a palindrome");
+//        } else {
+//            System.out.println("array " + Arrays.toString(arr) + " is not  a palindrome");
+//        }
+
+
+        // N4
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int n = in.nextInt();
+//        int[] arr = {1,2,5,1,2,4,1,2};
+//        int indexOfNewArr = 0;
+//
+//        for(int i : arr)
+//        {
+//            if(i != n)
+//            {
+//                indexOfNewArr++;
+//            }
+//        }
+//        int[] arrAfterRemoving = new int[indexOfNewArr];
+//        int a = 0;
+//        for(int i : arr)
+//        {
+//            if(i != n){
+//                arrAfterRemoving[a] = i;
+//                a++;
+//            }
+//        }
+//        System.out.println(Arrays.toString(arrAfterRemoving ) );
+    }
+    }
