8d2a6ac4a60cc4e9c6d38d037e3db483c3847369
anamaghr <maghradzeana1@gmail.com>
2026-01-29 18:12:09 +0400

    Add files via upload

---
+++ exception davaleba 1/src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        // N1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter first number: ");
+//        int n1 = scanner.nextInt();
+//        System.out.println("enter second number: ");
+//        int n2 = scanner.nextInt();
+//         try{
+//             int result = n1 / n2;
+//             System.out.println(n1 + " / " + n2 + " = " + result);
+//         }catch(ArithmeticException e)
+//         {
+//             System.out.println("arithmetical exception");
+//         }
+
+        // N2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter a number: ");
+//        String s = scanner.nextLine();
+//
+//        try{
+//            int n = Integer.parseInt(s);
+//            int result = 100 / n;
+//            System.out.println("100 / " + n + " = " + result);
+//        } catch (NumberFormatException e) {
+//            System.out.println("number format exception");
+//        }catch(ArithmeticException e)
+//        {
+//            System.out.println("arithmetical exception");
+//        }
+
+        // N3
+//        try{
+//            checkNumber(10);
+//        } catch (NegativeNumberException e) {
+//            System.out.println("exception: " + e.getMessage());
+//        }
+        // N4
+//        try{
+//            double result = calculateSquareRoot(9);
+//            System.out.println("square root: " + result);
+//        }catch (IllegalArgumentException e){
+//            System.out.println("exception: " + e.getMessage());
+//        }
+    }
+// N3
+//    public static void checkNumber(int n) throws NegativeNumberException{
+//        if (n < 0) {
+//            throw new NegativeNumberException("number can't be negative");
+//        }
+//        }
+
+    // N4
+//    public static double calculateSquareRoot(int n){
+//        if(n < 0)
+//        {
+//            throw new IllegalArgumentException("number can't be negative");
+//        }
+//        return Math.sqrt(n);
+//    }
+
+
+    }
