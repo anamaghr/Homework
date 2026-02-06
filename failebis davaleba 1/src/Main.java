e017ba37a6a265b71cb0303e4dcc124da70b7bc8
anamaghr <maghradzeana1@gmail.com>
2026-02-06 18:13:00 +0400

    Add files via upload

---
+++ failebis davaleba 1/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Main {
+    public static <text> void main(String[] args) throws IOException {
+
+        // N1
+//    String[] text = {"შიგან ასრე გავერივე, გნოლის ჯოგსა ვითა ქორი,",
+//                      "კაცი კაცსა შემოვსტყორცი, ცხენ-კაცისა დავდგი გორი;",
+//                    "კაცი. ჩემგან განატყორცი ბრუნავს ვითა ტანაჯორი,",
+//                       "ერთობ სრულად ამოვწყდე წინა კერძო რაზმი ორი",};
+//
+//    try(FileWriter writer = new FileWriter("text.txt")){
+//        for(String texts : text){
+//            writer.write(texts);
+//        }
+//        catch(IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+    }
+    }
