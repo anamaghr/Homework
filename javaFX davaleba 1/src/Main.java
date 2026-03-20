becec6f2958b57b6b32262341d34f1e65ae34f4d
anamaghr <maghradzeana1@gmail.com>
2026-03-20 18:39:51 +0400

    Add files via upload

---
+++ javaFX davaleba 1/src/Main.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+
+import java.awt.*;
+
+public class Main  extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // N1
+//        Group group = new Group();
+//        Label Label = new Label("hello, this is an application");
+//        group.getChildren().add(Label);
+//        Scene scene = new Scene(group,300,300, Color.AQUAMARINE);
+//        Label.setTextFill(Color.RED);
+//        Label.setAlignment(Pos.CENTER);
+//        stage.getIcons().add(new Image(getClass().getResourceAsStream("/black-cat.png")));
+//        stage.setTitle("text");
+//        stage.setScene(scene);
+//        stage.show();
+        
+    }
+}
